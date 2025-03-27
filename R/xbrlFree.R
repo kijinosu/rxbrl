b51c1917456cec586d1e20a99f91787e6a23ca56
Kijinosu <kijinosu@proton.me>
2025-03-27 15:43:39 +0900

    Update license and notices

--- R/xbrlFree.R
+++ R/xbrlFree.R
@@ -1,19 +1,20 @@
-## Copyright (C) 2014-2016 Roberto Bertolusso
+## rxbrl -  Extraction of Business Financial Information from 'XBRL' Documents
+## Copyright (C) 2014-2016 Roberto Bertolusso, 2025 Alan Engel
+
+## This file is part of rxbrl and was adapted from XBRL.
 ##
-## This file is part of XBRL.
-##
-## XBRL is free software: you can redistribute it and/or modify it
+## rxbrl is free software: you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by
 ## the Free Software Foundation, either version 2 of the License, or
 ## (at your option) any later version.
 ##
-## XBRL is distributed in the hope that it will be useful, but
+## rxbrl is distributed in the hope that it will be useful, but
 ## WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with XBRL. If not, see <http://www.gnu.org/licenses/>.
+## along with rxbrl. If not, see <http://www.gnu.org/licenses/>.
 
 
 xbrlFree <- function(doc) {
